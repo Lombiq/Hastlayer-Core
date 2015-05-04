7a454fee7b1a971dad3145cf4824fc48bc791e70
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2015-05-04 05:38:37 +0200

    VhdlBuilder: implementing generic and std_logic_vector
    --HG--
    branch : dev

---
+++ Hast.VhdlBuilder/Representation/Declaration/Generic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hast.VhdlBuilder.Extensions;
+using Hast.VhdlBuilder.Representation.Expression;
+
+namespace Hast.VhdlBuilder.Representation.Declaration
+{
+    [DebuggerDisplay("{ToVhdl()}")]
+    public class Generic : IVhdlElement
+    {
+        public List<GenericItem> Items { get; set; }
+
+
+        public string ToVhdl()
+        {
+            return
+                "generic (" +
+                (Items != null ? string.Join(";", Items.Select(item => item.ToVhdl())) : string.Empty) +
+                ");";
+        }
+    }
+
+
+    public class GenericItem : DataObjectBase
+    {
+        public Value Value { get; set; }
+
+
+        public GenericItem()
+        {
+            DataObjectKind = DataObjectKind.Constant;
+        }
+
+
+        override public string ToVhdl()
+        {
+            return
+                Name.ToExtendedVhdlId() +
+                ": " +
+                Value.DataType.ToVhdl() +
+                ":=" +
+                Value.ToVhdl();
+        }
+    }
+}
--- Hast.VhdlBuilder/Representation/Declaration/SizedDataType.cs
+++ Hast.VhdlBuilder/Representation/Declaration/SizedDataType.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace Hast.VhdlBuilder.Representation.Declaration
 {
@@ -6,12 +7,23 @@ namespace Hast.VhdlBuilder.Representation.Declaration
     public class SizedDataType : DataType
     {
         public int Size { get; set; }
+        public IVhdlElement SizeExpression { get; set; }
 
 
         public override string ToVhdl()
         {
-            if (Size == 0) return Name;
-            return Name + "(" + (Size - 1) + " downto 0)";
+            if (Size == 0 && SizeExpression == null) return Name;
+
+            if (Size != 0 && SizeExpression != null)
+            {
+                throw new InvalidOperationException("VHDL sized data types should have their size specified either as an integer value or as an expression, but not both.");
+            }
+
+            return
+                Name +
+                "(" +
+                (Size != 0 ? (Size - 1).ToString() : SizeExpression.ToVhdl()) +
+                " downto 0)";
         }
     }
 }
