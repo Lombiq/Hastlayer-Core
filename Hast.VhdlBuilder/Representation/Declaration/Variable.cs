8b21754d7febe082c8734d9bd3d95d8830f4f446
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2015-05-15 18:58:45 +0200

    Implementing shared VHDL variables
    --HG--
    branch : dev

--- Hast.VhdlBuilder/Representation/Declaration/Variable.cs
+++ Hast.VhdlBuilder/Representation/Declaration/Variable.cs
@@ -5,9 +5,18 @@ namespace Hast.VhdlBuilder.Representation.Declaration
     [DebuggerDisplay("{ToVhdl()}")]
     public class Variable : TypedDataObject
     {
+        public bool Shared { get; set; }
+
+
         public Variable()
         {
             DataObjectKind = DataObjectKind.Variable;
+        }
+
+
+        public override string ToVhdl()
+        {
+            return (Shared ? "shared " : string.Empty) + base.ToVhdl();
         }
     }
 }
