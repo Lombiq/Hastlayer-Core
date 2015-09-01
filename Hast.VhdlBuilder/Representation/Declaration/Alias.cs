0e590ab3ca05d8b052d5f8c79a410df07fc196b4
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2015-09-01 17:13:16 +0200

    Adding Alias VHDL element
    --HG--
    branch : feature/fsm-transformer

---
+++ Hast.VhdlBuilder/Representation/Declaration/Alias.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hast.VhdlBuilder.Representation.Declaration
+{
+    [DebuggerDisplay("{ToVhdl()}")]
+    public class Alias : TypedDataObjectBase
+    {
+        /// <summary>
+        /// Name of the object that the alias is created for.
+        /// </summary>
+        public string ObjectName { get; set; }
+
+
+        public Alias()
+        {
+            DataObjectKind = DataObjectKind.Variable;
+        }
+
+
+        public override string ToVhdl()
+        {
+            return
+                "alias " +
+                Name +
+                " : " +
+                 DataType.ToReferenceVhdl() +
+                " is " +
+                ObjectName +
+                ";";
+        }
+    }
+}
