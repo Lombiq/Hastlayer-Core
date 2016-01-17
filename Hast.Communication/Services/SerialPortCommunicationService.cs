672a1887cce9363446715caf428e2080991d81ea
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-01-17 02:02:20 +0100

    Handling in SerialPortCommunicationService that the received message length can now be bigger than 255, see HAST-45
    --HG--
    branch : dev

--- Hast.Communication/Services/SerialPortCommunicationService.cs
+++ Hast.Communication/Services/SerialPortCommunicationService.cs
@@ -87,12 +87,13 @@ namespace Hast.Communication.Services
                 serialPort.Write(outputBuffer, 0, outputBuffer.Length);
 
                 var taskCompletionSource = new TaskCompletionSource<bool>();
-                var messageBytesCount = 0; // The incoming byte buffer size.
-                var messageBytesReceived = 0; // Just used to know when the data is ready.
-                var returnValueBytes = new byte[0]; // The incoming buffer.
-                var returnValueIndex = 0;
                 var communicationType = Constants.SerialCommunicationConstants.Signals.Default;
-                var executionTimeClockCycles = new byte[4];
+                var messageBytesCountBytes = new byte[4];
+                var messageBytesCountByteCounter = 0;
+                var messageBytesCount = 0; // The incoming byte buffer size.
+                var messageBytesReceivedCount = 0; // Just used to know when the data is ready.
+                var receivedValueBytes = new byte[0]; // The incoming buffer.
+                var executionTimeClockCyclesBytes = new byte[4];
                 var executionTimeByteCounter = 0;
 
                 Action<byte, bool> processReceivedByte = (receivedByte, isLastOfBatch) =>
@@ -117,47 +118,51 @@ namespace Hast.Communication.Services
                         else if (communicationType == Constants.SerialCommunicationConstants.Signals.Information)
                         {
                             // We know that the incoming data's size will be 4 bytes.
-                            executionTimeClockCycles[executionTimeByteCounter] = receivedByte;
+                            executionTimeClockCyclesBytes[executionTimeByteCounter] = receivedByte;
                             executionTimeByteCounter++; // We increment the byte counter to index the next incoming byte.
                             if (executionTimeByteCounter == 4) // If we received the 4 bytes.
                             {
                                 // We switch the communication type back to 'result'.
                                 communicationType = Constants.SerialCommunicationConstants.Signals.Result;
                                 executionTimeByteCounter = 0;
-                                // Log the information.
-                                var executionTimeValue = BitConverter.ToUInt32(executionTimeClockCycles, 0);
+
+                                var executionTimeClockCycles = BitConverter.ToUInt32(executionTimeClockCyclesBytes, 0);
 
                                 // Hard-coding the divisor for now: the FPGA runs at 100Mhz so one clock cycle is 10ns.
-                                executionInformation.HardwareExecutionTimeMilliseconds = executionTimeValue / 100000;
+                                executionInformation.HardwareExecutionTimeMilliseconds = executionTimeClockCycles / 100000;
                                 Logger.Information("Hardware execution took " + executionInformation.HardwareExecutionTimeMilliseconds + "ms.");
                                 
                                 serialPort.Write(Constants.SerialCommunicationConstants.Signals.AllBytesReceived);
                             }
                         }
-                        else // If the communicationType variable is equal with Constants.FpgaConstants.Signals.Data (d) then this code will run.
+                        else // If the communicationType variable is equal to Constants.FpgaConstants.Signals.Data (d) then this code will run.
                         {
-                            // The first byte will be the size of the byte array that we must receive.
-                            if (messageBytesCount == 0) // To setup the right receiving buffer size.
+                            // The first bytes will be the size of the byte array that we must receive.
+                            if (messageBytesCount == 0)
                             {
-                                // The first byte is the data size that we must receive.
-                                messageBytesCount = receivedByte;
+                                messageBytesCountBytes[messageBytesCountByteCounter] = receivedByte;
+                                messageBytesCountByteCounter++;
 
-                                // Since the return value's size can differ from the input size for optimization reasons,
-                                // we take the explicit size into account.
-                                returnValueBytes = new byte[messageBytesCount];
+                                if (messageBytesCountByteCounter == 4)
+                                {
+                                    messageBytesCount = BitConverter.ToInt32(messageBytesCountBytes, 0);
 
-                                Logger.Information("Incoming data size: {0}", Convert.ToChar(messageBytesCount).ToString());
+                                    // Since the return value's size can differ from the input size for optimization reasons,
+                                    // we take the explicit size into account.
+                                    receivedValueBytes = new byte[messageBytesCount];
 
-                                // Signal that we are ready to receive the data.
-                                serialPort.Write(Constants.SerialCommunicationConstants.Signals.Ready);
+                                    Logger.Information("Incoming data size: {0}", Convert.ToChar(messageBytesCount).ToString());
+
+                                    // Signal that we are ready to receive the data.
+                                    serialPort.Write(Constants.SerialCommunicationConstants.Signals.Ready); 
+                                }
                             }
                             else
                             {
                                 // We put together the received data stream.
 
-                                returnValueBytes[returnValueIndex] = receivedByte;
-                                returnValueIndex++;
-                                messageBytesReceived++;
+                                receivedValueBytes[messageBytesReceivedCount] = receivedByte;
+                                messageBytesReceivedCount++;
                                 if (isLastOfBatch)
                                 {
                                     serialPort.Write(Constants.SerialCommunicationConstants.Signals.Ready); 
@@ -165,12 +170,12 @@ namespace Hast.Communication.Services
                             }
 
                             // Set the incoming data if all bytes are received (waiting for incoming data stream to complete).
-                            if (messageBytesCount == messageBytesReceived)
+                            if (messageBytesCount > 0 && messageBytesCount == messageBytesReceivedCount)
                             {
                                 // Signal that we received all bytes.
                                 serialPort.Write(Constants.SerialCommunicationConstants.Signals.AllBytesReceived);
 
-                                simpleMemory.Memory = returnValueBytes;
+                                simpleMemory.Memory = receivedValueBytes;
                                 taskCompletionSource.SetResult(true);
 
                             }
