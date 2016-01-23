ee1cefc6c28493c773c015b6784ea9ef75d666cf
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-01-23 16:08:29 +0100

    Introducing hardware drivers for board-specific things
    --HG--
    branch : dev

---
+++ Hast.Synthesis/IHardwareDriver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hast.Synthesis.Models;
+using Orchard;
+
+namespace Hast.Synthesis
+{
+    /// <summary>
+    /// Provides FPGA-specific implementations.
+    /// </summary>
+    public interface IHardwareDriver : IDependency
+    {
+        IHardwareManifest HardwareManifest { get; }
+    }
+}
