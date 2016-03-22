57ba84fea3b8512e44a4eb72b9b5a8de4902bef3
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-03-22 17:05:09 +0100

    Running AST transformations so delegates are visible in the AST
    --HG--
    branch : issue/HAST-55

--- Hast.Transformer/DefaultTransformer.cs
+++ Hast.Transformer/DefaultTransformer.cs
@@ -11,6 +11,7 @@ using Hast.Transformer.Models;
 using Hast.Transformer.Services;
 using ICSharpCode.Decompiler;
 using ICSharpCode.Decompiler.Ast;
+using ICSharpCode.Decompiler.Ast.Transforms;
 using ICSharpCode.NRefactory.CSharp;
 using Mono.Cecil;
 using Orchard.Services;
@@ -48,7 +49,8 @@ namespace Hast.Transformer
         {
             var firstAssembly = AssemblyDefinition.ReadAssembly(Path.GetFullPath(assemblyPaths.First()));
             var transformationId = firstAssembly.FullName;
-            var astBuilder = new AstBuilder(new DecompilerContext(firstAssembly.MainModule));
+            var decompiledContext = new DecompilerContext(firstAssembly.MainModule);
+            var astBuilder = new AstBuilder(decompiledContext);
             astBuilder.AddAssembly(firstAssembly);
 
             foreach (var assemblyPath in assemblyPaths.Skip(1))
@@ -64,7 +66,67 @@ namespace Hast.Transformer
                 _jsonConverter.Serialize(configuration.CustomConfiguration);
 
 
+            // astBuilder.RunTransformations() is needed for the syntax tree to be ready, 
+            // see: https://github.com/icsharpcode/ILSpy/issues/686. But we can't run that directly since that would
+            // also transform some low-level constructs that are useful to have as simple as possible (e.g. it's OK if
+            // we only have while statements in the AST, not for statements mixed in). So we need to remove the unuseful
+            // pipeline steps and run them by hand.
             var syntaxTree = astBuilder.SyntaxTree;
+            IEnumerable<IAstTransform> pipeline = TransformationPipeline.CreatePipeline(decompiledContext);
+            // We allow the commented out pipeline steps. Must revisit after ILSpy update.
+            pipeline = pipeline
+                // Converts e.g. !num6 == 0 expression to num6 != 0 and other simplifications.
+                //.Without("PushNegation")
+
+                // Re-creates delegates e.g. from compiler-generated DisplayClasses.
+                //.Without("DelegateConstruction")
+
+                // Re-creates e.g. for statements from while statements.
+                .Without("PatternStatementTransform")
+
+                // Converts e.g. num6 = num6 + 1; to num6 += 1.
+                .Without("ReplaceMethodCallsWithOperators")
+
+                // Deals with the unsafe modifier but we don't support PInvoke any way.
+                .Without("IntroduceUnsafeModifier")
+
+                // Re-adds checked() blocks that are used for compile-time overflow checking in C#, see:
+                // https://msdn.microsoft.com/en-us/library/74b4xzyw.aspx. We don't need this for transformation.
+                .Without("AddCheckedBlocks")
+
+                // Merges separate variable declarations with variable initializations what would make transformation
+                // more complicated.
+                .Without("DeclareVariables")
+
+                // Removes empty ctors or ctors that can be subsctituted with field initializers.
+                //.Without("ConvertConstructorCallIntoInitializer")
+
+                // Converts decimal const fields to more readable variants, e.g. this:
+                // [DecimalConstant (0, 0, 0u, 0u, 234u)]
+                // private static readonly decimal a = 234m;
+                // To this (which is closer to the original):
+                // private const decimal a = 234m;
+                //.Without("DecimalConstantTransform")
+
+                // Adds using declarations that aren't needed for transformation.
+                .Without("IntroduceUsingDeclarations")
+
+                // Converts ExtensionsClass.ExtensionMethod(this) calls to this.ExtensionMethod(). This would make
+                // the future transformation of extension methods difficult, since this makes them look like instance
+                // methods (however those instance methods don't exist).
+                .Without("IntroduceExtensionMethods")
+
+                // These two deal with LINQ elements that we don't support yet any way.
+                .Without("IntroduceQueryExpressions")
+                .Without("CombineQueryExpressions")
+
+                // Removes an unnecessary BlockStatement level from switch statements.
+                //.Without("FlattenSwitchBlocks")
+                ;
+            foreach (var transform in pipeline)
+            {
+                transform.Run(syntaxTree);
+            }
 
 
             _syntaxTreeCleaner.CleanUnusedDeclarations(syntaxTree, configuration);
---
+++ Hast.Transformer/Extensions/AstTransformEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.Decompiler.Ast.Transforms;
+
+namespace System.Collections.Generic
+{
+    internal static class AstTransformEnumerableExtensions
+    {
+        public static IEnumerable<IAstTransform> Without(this IEnumerable<IAstTransform> enumerable, string className)
+        {
+            return enumerable.Where(item => item.GetType().Name != className);
+        }
+    }
+}
