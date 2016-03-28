94d1b204a7b135d6df8e77d347e352884cf8241e
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-03-28 23:37:37 +0200

    A bit of restructuring
    --HG--
    branch : issue/HAST-55

--- Hast.Transformer.Vhdl/SubTransformers/CompilerGeneratedClassesConverter.cs
+++ Hast.Transformer.Vhdl/SubTransformers/CompilerGeneratedClassesConverter.cs
@@ -35,7 +35,6 @@ namespace Hast.Transformer.Vhdl.SubTransformers
                 }
              */
 
-            // Processing the DisplayClasses themselves.
             var compilerGeneratedClasses = syntaxTree
                 .GetAllTypeDeclarations()
                 .Where(type => type.ClassType == ClassType.Class && type.Name.Contains("__DisplayClass"))
@@ -43,102 +42,111 @@ namespace Hast.Transformer.Vhdl.SubTransformers
                     .Attributes
                     .Any(attributeSection => attributeSection
                         .Attributes.Any(attribute => attribute.Type.GetSimpleName() == "CompilerGeneratedAttribute")));
-            var thisFieldNames = new HashSet<string>();
             foreach (var compilerGeneratedClass in compilerGeneratedClasses)
             {
-                // Removing __this references.
-                var thisField = compilerGeneratedClass.Members
-                    .SingleOrDefault(member =>
-                        member is FieldDeclaration &&
-                        ((FieldDeclaration)member).Variables.Any(variable => variable.Name.EndsWith("__this")));
-                if (thisField != null)
+                var thisFieldNames = new HashSet<string>();
+
+
+                // Processing the DisplayClasses themselves.
                 {
-                    var fieldName = ((FieldDeclaration)thisField).Variables.First().Name;
-                    thisFieldNames.Add(fieldName);
-                    thisField.Remove();
-                    Action<MemberReferenceExpression> memberReferenceExpressionProcessor = memberReferenceExpression =>
-                        {
-                            if (memberReferenceExpression.MemberName == fieldName)
+                    // Removing __this references.
+                    var thisField = compilerGeneratedClass.Members
+                        .SingleOrDefault(member =>
+                            member is FieldDeclaration &&
+                            ((FieldDeclaration)member).Variables.Any(variable => variable.Name.EndsWith("__this")));
+                    if (thisField != null)
+                    {
+                        var fieldName = ((FieldDeclaration)thisField).Variables.First().Name;
+
+                        thisFieldNames.Add(fieldName);
+                        thisField.Remove();
+
+                        Action<MemberReferenceExpression> memberReferenceExpressionProcessor = memberReferenceExpression =>
                             {
-                                // The parent should also be a MemberReferenceExpression since there is no other type of access
-                                // to __this fields.
-                                var parentMemberReferenceExpression = (MemberReferenceExpression)memberReferenceExpression.Parent;
-                                parentMemberReferenceExpression.Target = memberReferenceExpression.Target;
-                                memberReferenceExpression.Remove();
-                            }
-                        };
-                    compilerGeneratedClass.AcceptVisitor(new MemberReferenceExpressionVisitingVisitor(memberReferenceExpressionProcessor));
-                }
+                                if (memberReferenceExpression.MemberName == fieldName)
+                                {
+                                    // The parent should also be a MemberReferenceExpression since there is no other type of access
+                                    // to __this fields.
+                                    var parentMemberReferenceExpression = (MemberReferenceExpression)memberReferenceExpression.Parent;
+                                    parentMemberReferenceExpression.Target = memberReferenceExpression.Target;
+                                    memberReferenceExpression.Remove();
+                                }
+                            };
+                        compilerGeneratedClass
+                            .AcceptVisitor(new MemberReferenceExpressionVisitingVisitor(memberReferenceExpressionProcessor));
+                    }
 
 
-                // Converting fields into method arguments.
-                var fields = compilerGeneratedClass.Members.OfType<FieldDeclaration>()
-                    .OrderBy(field => field.Variables.Single().Name)
-                    .ToArray(); // Eagerly evaluating so the below removal doesn't affect this field collection.
+                    // Converting fields into method arguments.
+                    var fields = compilerGeneratedClass.Members.OfType<FieldDeclaration>()
+                        .OrderBy(field => field.Variables.Single().Name)
+                        .ToArray(); // Eagerly evaluating so the below removal doesn't affect this field collection.
 
-                foreach (var field in fields)
-                {
-                    field.Remove();
-                }
+                    foreach (var field in fields)
+                    {
+                        field.Remove();
+                    }
 
-                foreach (var method in compilerGeneratedClass.Members.OfType<MethodDeclaration>())
-                {
-                    // Adding parameters for every field that the method used, in alphabetical order, and changing field
-                    // references to parameter references.
+                    foreach (var method in compilerGeneratedClass.Members.OfType<MethodDeclaration>())
+                    {
+                        // Adding parameters for every field that the method used, in alphabetical order, and changing field
+                        // references to parameter references.
 
-                    var parametersForFormerFields = new SortedList<string, ParameterDeclaration>();
+                        var parametersForFormerFields = new SortedList<string, ParameterDeclaration>();
 
-                    Action<MemberReferenceExpression> memberReferenceExpressionProcessor = memberReferenceExpression =>
-                        {
-                            var fieldDefinition = memberReferenceExpression.Annotation<FieldDefinition>();
-
-                            if (fieldDefinition == null) return;
-
-                            var field = fields.Single(f => f.Annotation<FieldDefinition>().FullName == fieldDefinition.FullName);
-
-                            // Is the field assigned to? Because we don't support that currently, since with it being
-                            // converted to a parameter we'd need to return its value and assign it to the caller's
-                            // variable. Maybe we'll allow this with static field support, but not for lambdas used
-                            // in parallelized expressions (since that would require concurrent access too).
-                            var isAssignedTo = 
-                                // The field is directly assigned to.
-                                (memberReferenceExpression.Parent is AssignmentExpression &&
-                                ((AssignmentExpression)memberReferenceExpression.Parent).Left == memberReferenceExpression)
-                                ||
-                                // The field's indexed element is assigned to.
-                                (memberReferenceExpression.Parent is IndexerExpression &&
-                                memberReferenceExpression.Parent.Parent is AssignmentExpression &&
-                                ((AssignmentExpression)memberReferenceExpression.Parent.Parent).Left == memberReferenceExpression.Parent);
-                            if (isAssignedTo)
+                        Action<MemberReferenceExpression> memberReferenceExpressionProcessor = memberReferenceExpression =>
                             {
-                                throw new NotSupportedException("It's not supported to modify the content of a variable coming from the parent scope in a lambda expression. Pass arguments instead.");
-                            }
+                                var fieldDefinition = memberReferenceExpression.Annotation<FieldDefinition>();
 
-                            // This generated name should be sufficiently unique so it doesn't clash with existing
-                            // parameters.
-                            var parameterName = compilerGeneratedClass.Name + "_" + field.Variables.Single().Name;
+                                if (fieldDefinition == null) return;
 
-                            // Adding a method parameter for the field.
-                            parametersForFormerFields.Add(
-                                parameterName,
-                                new ParameterDeclaration(field.ReturnType.Clone(), parameterName));
+                                var field = fields.Single(f => f.Annotation<FieldDefinition>().FullName == fieldDefinition.FullName);
 
-                            // Changing the field reference to a reference to the parameter.
-                            //var fieldTypeReference = field.ReturnType.GetActualTypeReference();
-                            var identifierExpression = new IdentifierExpression(parameterName);
-                            //identifierExpression.AddAnnotation(new TypeInformation(fieldTypeReference, fieldTypeReference));
-                            memberReferenceExpression.ReplaceWith(identifierExpression);
-                        };
-                    method.AcceptVisitor(new MemberReferenceExpressionVisitingVisitor(memberReferenceExpressionProcessor));
+                                // Is the field assigned to? Because we don't support that currently, since with it being
+                                // converted to a parameter we'd need to return its value and assign it to the caller's
+                                // variable. Maybe we'll allow this with static field support, but not for lambdas used
+                                // in parallelized expressions (since that would require concurrent access too).
+                                var isAssignedTo =
+                                    // The field is directly assigned to.
+                                    (memberReferenceExpression.Parent is AssignmentExpression &&
+                                    ((AssignmentExpression)memberReferenceExpression.Parent).Left == memberReferenceExpression)
+                                    ||
+                                    // The field's indexed element is assigned to.
+                                    (memberReferenceExpression.Parent is IndexerExpression &&
+                                    memberReferenceExpression.Parent.Parent is AssignmentExpression &&
+                                    ((AssignmentExpression)memberReferenceExpression.Parent.Parent).Left == memberReferenceExpression.Parent);
+                                if (isAssignedTo)
+                                {
+                                    throw new NotSupportedException("It's not supported to modify the content of a variable coming from the parent scope in a lambda expression. Pass arguments instead.");
+                                }
 
-                    method.Parameters.AddRange(parametersForFormerFields.Values);
+                                // This generated name should be sufficiently unique so it doesn't clash with existing
+                                // parameters.
+                                var parameterName = compilerGeneratedClass.Name + "_" + field.Variables.Single().Name;
+
+                                // Adding a method parameter for the field.
+                                parametersForFormerFields.Add(
+                                    parameterName,
+                                    new ParameterDeclaration(field.ReturnType.Clone(), parameterName));
+
+                                // Changing the field reference to a reference to the parameter.
+                                //var fieldTypeReference = field.ReturnType.GetActualTypeReference();
+                                var identifierExpression = new IdentifierExpression(parameterName);
+                                //identifierExpression.AddAnnotation(new TypeInformation(fieldTypeReference, fieldTypeReference));
+                                memberReferenceExpression.ReplaceWith(identifierExpression);
+                            };
+                        method.AcceptVisitor(new MemberReferenceExpressionVisitingVisitor(memberReferenceExpressionProcessor));
+
+                        method.Parameters.AddRange(parametersForFormerFields.Values);
+                    }
                 }
-            }
 
+                // Processing consumer code of DisplayClasses.
+                {
+                    var parentClass = compilerGeneratedClass.Parent;
 
-            // Processing consumer code of DisplayClasses.
-            {
-                Action<MemberReferenceExpression> memberReferenceExpressionProcessor = memberReferenceExpression =>
+                    // Removing __this field references.
+                    Action<MemberReferenceExpression> memberReferenceExpressionProcessor = memberReferenceExpression =>
                     {
                         if (thisFieldNames.Contains(memberReferenceExpression.MemberName))
                         {
@@ -147,7 +155,8 @@ namespace Hast.Transformer.Vhdl.SubTransformers
                             memberReferenceExpression.FindFirstParentOfType<ExpressionStatement>().Remove();
                         }
                     };
-                syntaxTree.AcceptVisitor(new MemberReferenceExpressionVisitingVisitor(memberReferenceExpressionProcessor));
+                    parentClass.AcceptVisitor(new MemberReferenceExpressionVisitingVisitor(memberReferenceExpressionProcessor));
+                }
             }
         }
 
