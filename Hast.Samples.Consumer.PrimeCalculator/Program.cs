5716796c62dd3e0b71aec42c14761f498ce29afe
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-04-08 18:59:48 +0200

    Adding notes about Ethernet speeds
    --HG--
    branch : issue/HAST-40

--- Hast.Samples.Consumer.PrimeCalculator/Program.cs
+++ Hast.Samples.Consumer.PrimeCalculator/Program.cs
@@ -48,6 +48,8 @@ namespace Hast.Samples.Consumer.PrimeCalculator
                     // With 210 numbers this takes about 2,1s all together (with UART) on an FPGA and 166s on a 3,2GHz i7.
                     // With 4000 numbers it takes 38s on an FPGA and 3550s (about an hour) on the same PC. 10000 numbers
                     // take 84s on an FPGA.
+                    // These take the following amount of time via Ethernet respectively: 330ms (200 numbers), 1,5s 
+                    // (4000 numbers), 6,8s (10000 numbers).
                     // About 90000000 numbers are the maximum before an OutOfMemoryException down the line. But that would
                     // take 93 hours to send via 9600 baud serial (and then above this to receive the results).
                     var numberCount = 4000;
