e50af66cc9a2efe79b7f95c2fcba932c0d77b206
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-05-31 01:58:58 +0200

    Omitting the static field sometimes generated for lambda expressions
    --HG--
    branch : issue/HAST-55

--- Hast.Transformer.Vhdl/SubTransformers/DisplayClassFieldTransformer.cs
+++ Hast.Transformer.Vhdl/SubTransformers/DisplayClassFieldTransformer.cs
@@ -43,7 +43,10 @@ namespace Hast.Transformer.Vhdl.SubTransformers
                     !field.Variables.Any(variable => variable.Name.EndsWith("__this")) &&
                     // Roslyn adds a field like public Func<object, bool> <>9__0; with the same argument and return types 
                     // as the original lambda. Nothing needs to be done with this.
-                    !(field.ReturnType.Is<SimpleType>(simple => simple.Identifier == "Func"));
+                    !(field.ReturnType.Is<SimpleType>(simple => simple.Identifier == "Func")) &&
+                    // Sometimes the compiler adds a static field containing an object of the parent class as below:
+                    // public static readonly HastlayerOptimizedAlgorithm.<>c <>9 = new HastlayerOptimizedAlgorithm.<>c ();
+                    field.Modifiers != (Modifiers.Public | Modifiers.Static | Modifiers.Readonly);
                 if (shouldTransform)
                 {
                     var dataType = _typeConverter.ConvertAstType(field.ReturnType);
