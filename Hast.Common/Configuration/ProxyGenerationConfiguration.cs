69e02ef47b4dd32ba016ec6ef8fded5a08ca4b68
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-07-17 20:46:37 +0200

    Making the serial communication channel the default for now
    --HG--
    branch : issue/HAST-55

--- Hast.Common/Configuration/ProxyGenerationConfiguration.cs
+++ Hast.Common/Configuration/ProxyGenerationConfiguration.cs
@@ -24,7 +24,7 @@ namespace Hast.Common.Configuration
                 {
                     _default = new ProxyGenerationConfiguration
                     {
-                        CommunicationChannelName = "Ethernet"
+                        CommunicationChannelName = "Serial"
                     };
                 }
 
