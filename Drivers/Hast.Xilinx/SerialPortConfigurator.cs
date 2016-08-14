29e28b8b49dd15426045276342556b479cff1c8f
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-08-14 21:22:33 +0200

    Increasing UART speed to 230400 baud as it works without issues
    460800 baud was too fast, the board couldn't really keep up with receiving a lot
    of data properly.
    
    --HG--
    branch : dev

---
+++ Drivers/Hast.Xilinx/SerialPortConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hast.Common.Extensibility.Pipeline;
+using Hast.Communication.Extensibility.Pipeline;
+
+namespace Hast.Xilinx
+{
+    public class SerialPortConfigurator : PipelineStepBase, ISerialPortConfigurator
+    {
+        public void ConfigureSerialPort(SerialPort serialPort)
+        {
+            serialPort.BaudRate = 230400;
+        }
+    }
+}
