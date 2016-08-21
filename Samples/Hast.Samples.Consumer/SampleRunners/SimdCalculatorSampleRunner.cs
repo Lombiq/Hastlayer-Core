3d97a77f00ffac06615aaee3fb3846810c9987f8
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-08-21 22:05:44 +0200

    Adding error checker for the SimdCalculator samples
    --HG--
    branch : dev

--- Samples/Hast.Samples.Consumer/SampleRunners/SimdCalculatorSampleRunner.cs
+++ Samples/Hast.Samples.Consumer/SampleRunners/SimdCalculatorSampleRunner.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Hast.Common.Configuration;
 using Hast.Common.Models;
@@ -21,10 +22,41 @@ namespace Hast.Samples.Consumer.SampleRunners
 
             var simdCalculator = await hastlayer.GenerateProxy(hardwareRepresentation, new SimdCalculator());
 
-            var sumVector = simdCalculator.AddVectors(vector, vector);
-            var differenceVector = simdCalculator.SubtractVectors(vector, vector);
-            var productVector = simdCalculator.MultiplyVectors(vector, vector);
-            var quotientVector = simdCalculator.DivideVectors(vector, vector);
+            var sumVector = ThrowIfNotCorrect(simdCalculator, calculator => calculator.AddVectors(vector, vector));
+            var differenceVector = ThrowIfNotCorrect(simdCalculator, calculator => calculator.SubtractVectors(vector, vector));
+            var productVector = ThrowIfNotCorrect(simdCalculator, calculator => calculator.MultiplyVectors(vector, vector));
+            var quotientVector = ThrowIfNotCorrect(simdCalculator, calculator => calculator.DivideVectors(vector, vector));
+        }
+
+
+        // Checking if the hardware result is correct by running it agains the software implementation.
+        private static int[] ThrowIfNotCorrect(SimdCalculator proxied, Func<SimdCalculator, int[]> operation)
+        {
+            var hardwareResult = operation(proxied);
+            var softwareResult = operation(new SimdCalculator());
+
+            if (hardwareResult.Length != softwareResult.Length)
+            {
+                throw new InvalidOperationException("The two results' length is not the same.");
+            }
+
+            for (int i = 0; i < hardwareResult.Length; i++)
+            {
+                try
+                {
+                    if (hardwareResult[i] != softwareResult[i])
+                    {
+                        throw new InvalidOperationException(
+                            "The hardware result and the software result is not the same on index " + i.ToString() + ": " +
+                            hardwareResult[i].ToString() + " vs " + softwareResult[i].ToString() + ".");
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            return hardwareResult;
         }
     }
 }
