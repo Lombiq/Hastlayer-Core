03941db095dbb207eb09c759acc231461444e1ca
Benedek Farkas <benedek.farkas@lombiq.com>
2016-11-19 23:04:55 +0100

    Further Unum scaffolding
    --HG--
    branch : issue/HAST-90

--- Hast.Common/Numerics/Unum/Unum.cs
+++ Hast.Common/Numerics/Unum/Unum.cs
@@ -1,41 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace Hast.Common.Numerics.Unum
 {
     public struct Unum// : IComparable, IFormattable, IConvertible, IComparable<Unum>, IEquatable<Unum>
     {
         #region Unum structure
+        private byte _exponentSizeSize;
+        private byte _fractionSizeSize;
+
+        private byte _exponentSizeMax;
+        private byte _fractionSizeMax;
+
+        private byte _unumTagSize;
+        private byte _size;
+        #endregion
+
+        #region Unum blocks
         private bool _signBit;
         private bool[] _exponentBits;
         private bool[] _fractionBits;
-        private bool _uBit;
+
+        private bool _uncertaintyBit;
         private bool[] _exponentSizeBits;
         private bool[] _fractionSizeBits;
         #endregion
 
-        private byte _size;
-        private byte _uTagSize;
-        private bool[] _uTag => new[] { _uBit }.Union(_exponentSizeBits).Union(_fractionSizeBits).ToArray();
+        #region Unum masks
+        private int _uncertaintyBitMask;
+        private int _exponentSizeMask;
+        private int _fractionSizeMask;
+        private int _exponentAndFractionSizeMask;
+        private int _unumTagMask;
+        #endregion
+
+        #region Unum metadata
+        private int _uLP; // Unit in the Last Place or Unit of Least Precision.
+
+        //private int _positiveInfinityUnum;
+        //private int _negativeInfinityUnum;
+
+        //private int _quietNotANumberUnum;
+        //private int _signalingNotANumberUnum;
+
+        //private int _maxRealUnum; // Largest representable positive number. One ULP less than infinity.
+        //private int _smallSubNormalUnum; // Smallest representable positive number. One ULP more than 0.
+
+        //private int _negativeBiggestUnum; // Smallest representable negative number. One ULP more than negative infinity.
+
+        #endregion
 
 
-        public Unum(byte eSize, byte fSize)
+        public Unum(byte exponentSizeSize, byte fractionSizeSize)
         {
-            var eSizeSize = UnumHelper.SegmentSizeToSegmentSizeSize(eSize);
-            var fSizeSize = UnumHelper.SegmentSizeToSegmentSizeSize(fSize);
+            // Initializing structure.
+            _exponentSizeSize = exponentSizeSize;
+            _fractionSizeSize = fractionSizeSize;
 
+            _exponentSizeMax = (byte)(1 << _exponentSizeSize);
+            _fractionSizeMax = (byte)(1 << _fractionSizeSize);
+
+            _unumTagSize = (byte)(1 + _exponentSizeSize + _fractionSizeSize);
+            _size = (byte)(1 + _exponentSizeMax + _fractionSizeMax + _unumTagSize);
+
+            // Initializing blocks.
             _signBit = false;
-            _exponentBits = new bool[eSize];
-            _fractionBits = new bool[fSize];
-            _uBit = false;
-            _exponentSizeBits = new bool[eSizeSize];
-            _fractionSizeBits = new bool[fSizeSize];
+            _uncertaintyBit = false;
 
-            _uTagSize = (byte)(1 + eSizeSize + fSizeSize);
-            _size = (byte)(1 + eSizeSize + fSizeSize + _uTagSize);
+            _exponentBits = new bool[_exponentSizeMax];
+            _fractionBits = new bool[_fractionSizeMax];
+
+            _exponentSizeBits = new bool[_exponentSizeSize];
+            _fractionSizeBits = new bool[_fractionSizeSize];
+
+            // Initializing masks.
+            _uncertaintyBitMask = 1 << (_unumTagSize - 1);
+            _fractionSizeMask = (1 << _fractionSizeMax) - 1;
+            _exponentSizeMask = (_uncertaintyBitMask - 1) - _fractionSizeMask;
+            _exponentAndFractionSizeMask = _exponentSizeMask | _fractionSizeMask;
+            _unumTagMask = _uncertaintyBitMask | _exponentAndFractionSizeMask;
+
+            // Initializing metadata.
+            _uLP = 1 << _unumTagSize;
+
         }
 
 
