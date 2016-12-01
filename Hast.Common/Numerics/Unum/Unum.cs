a426142f98d93a62b6910afc1fc59abe38dac363
Benedek Farkas <benedek.farkas@lombiq.com>
2016-12-01 02:32:44 +0100

    Added almost all necessary fields and initializing them
    --HG--
    branch : issue/HAST-90

--- Hast.Common/Numerics/Unum/Unum.cs
+++ Hast.Common/Numerics/Unum/Unum.cs
@@ -1,18 +1,16 @@
-﻿using System.Linq;
-
-namespace Hast.Common.Numerics.Unum
+﻿namespace Hast.Common.Numerics.Unum
 {
     public struct Unum// : IComparable, IFormattable, IConvertible, IComparable<Unum>, IEquatable<Unum>
     {
         #region Unum structure
-        private byte _exponentSizeSize;
-        private byte _fractionSizeSize;
+        private byte _exponentSizeSize; // "esizesize"
+        private byte _fractionSizeSize; // "fsizesize"
 
-        private byte _exponentSizeMax;
-        private byte _fractionSizeMax;
+        private byte _exponentSizeMax; // "esizemax"
+        private byte _fractionSizeMax; // "fsizemax"
 
-        private byte _unumTagSize;
-        private byte _size;
+        private byte _unumTagSize; // "utagsize"
+        private byte _size; // "maxubits"
         #endregion
 
         #region Unum blocks
@@ -26,27 +24,35 @@ namespace Hast.Common.Numerics.Unum
         #endregion
 
         #region Unum masks
-        private int _uncertaintyBitMask;
-        private int _exponentSizeMask;
-        private int _fractionSizeMask;
-        private int _exponentAndFractionSizeMask;
-        private int _unumTagMask;
+        private uint _uncertaintyBitMask; // "ubitmask"
+        private uint _exponentSizeMask; // "esizemask"
+        private uint _fractionSizeMask; // "fsizemask"
+        private uint _exponentAndFractionSizeMask; // "efsizemask"
+        private uint _unumTagMask; // "utagmask"
         #endregion
 
         #region Unum metadata
-        private int _uLP; // Unit in the Last Place or Unit of Least Precision.
+        // Bit masks should be at least of size "_unumTagSize + 1", instead of fixed 32 bits.
+        // The precision where it could matter is ridiculously high though, so it shouldn't matter for now.
 
-        //private int _positiveInfinityUnum;
-        //private int _negativeInfinityUnum;
+        private uint _uLP; // Unit in the Last Place or Unit of Least Precision.
 
-        //private int _quietNotANumberUnum;
-        //private int _signalingNotANumberUnum;
+        private uint _positiveInfinity; // "posinfu", the positive infinity for the given unum environment.
+        private uint _negativeInfinity; // "neginfu", the negative infinity for the given unum environment.
 
-        //private int _maxRealUnum; // Largest representable positive number. One ULP less than infinity.
-        //private int _smallSubNormalUnum; // Smallest representable positive number. One ULP more than 0.
+        private uint _quietNotANumber; // "qNaNu"
+        private uint _signalingNotANumber; // "sNaNu"
 
-        //private int _negativeBiggestUnum; // Smallest representable negative number. One ULP more than negative infinity.
+        private uint _largestPositive; // "maxrealu", the largest magnitude positive real number. One ULP less than infinity.
+        private uint _smallestPositive; // "smallsubnormalu", the smallest magnitude positive real number. One ULP more than 0.
 
+        private uint _largestNegative; // "negbigu", the largest maginude negative real number. One ULP more than negative infinity.
+        private uint _minrealu; // "minrealu", looks like to be exactly the same as "negbigu".
+
+        // uint is too small for a not particularly big unum, to be refactored.
+        private uint _signBitOne; // "signbigu", a unum in which all bits are zero except the sign bit;
+
+        //private uint _smallNormal; // "smallnormalu"
         #endregion
 
 
@@ -73,15 +79,31 @@ namespace Hast.Common.Numerics.Unum
             _fractionSizeBits = new bool[_fractionSizeSize];
 
             // Initializing masks.
-            _uncertaintyBitMask = 1 << (_unumTagSize - 1);
-            _fractionSizeMask = (1 << _fractionSizeMax) - 1;
+            _uncertaintyBitMask = ((uint)1 << (_unumTagSize - 1));
+            _fractionSizeMask = (((uint)1 << _fractionSizeMax) - 1);
             _exponentSizeMask = (_uncertaintyBitMask - 1) - _fractionSizeMask;
             _exponentAndFractionSizeMask = _exponentSizeMask | _fractionSizeMask;
             _unumTagMask = _uncertaintyBitMask | _exponentAndFractionSizeMask;
 
             // Initializing metadata.
-            _uLP = 1 << _unumTagSize;
+            _uLP = (uint)1 << _unumTagSize;
 
+            _signBitOne = (uint)1 << _size - 1;
+
+            _positiveInfinity = _signBitOne - 1 - _uncertaintyBitMask;
+            _negativeInfinity = _positiveInfinity + _signBitOne;
+
+            _largestPositive = _positiveInfinity - _uLP;
+            _smallestPositive = _exponentAndFractionSizeMask + _uLP;
+
+            _largestNegative = _negativeInfinity - _uLP;
+
+            _minrealu = _largestPositive + _signBitOne;
+
+            _quietNotANumber = _positiveInfinity + _uncertaintyBitMask;
+            _signalingNotANumber = _negativeInfinity + _uncertaintyBitMask;
+
+            //_smallNormal = _exponentAndFractionSizeMask + 1 << _size - 1 - _exponentSizeMax;
         }
 
 
