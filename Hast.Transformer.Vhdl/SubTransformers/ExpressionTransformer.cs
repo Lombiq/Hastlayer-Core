3fafc4526e1b326e38fd526224f273550370e315
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-12-24 04:29:29 +0100

    Implementing object initializer support
    --HG--
    branch : issue/HAST-103

--- Hast.Transformer.Vhdl/SubTransformers/ExpressionTransformer.cs
+++ Hast.Transformer.Vhdl/SubTransformers/ExpressionTransformer.cs
@@ -541,7 +541,31 @@ namespace Hast.Transformer.Vhdl.SubTransformers
             }
             else if (expression is ObjectCreateExpression)
             {
-                InitializeRecord(expression, ((ObjectCreateExpression)expression).Type, context);
+                var objectCreateExpression = (ObjectCreateExpression)expression;
+
+                var initiailizationResult = InitializeRecord(expression, objectCreateExpression.Type, context);
+
+                if (objectCreateExpression.Initializer.Elements.Any())
+                {
+                    foreach (var initializerElement in objectCreateExpression.Initializer.Elements)
+                    {
+                        var namedInitializerExpression = initializerElement as NamedExpression;
+                        if (namedInitializerExpression == null)
+                        {
+                            throw new NotSupportedException("Object initializers can only contain named expressions (i.e. \"Name = expression\" pairs).");
+                        }
+
+                        context.Scope.CurrentBlock.Add(new Assignment
+                        {
+                            AssignTo = new RecordFieldAccess
+                            {
+                                Instance = initiailizationResult.RecordInstanceReference,
+                                FieldName = namedInitializerExpression.Name.ToExtendedVhdlId()
+                            },
+                            Expression = Transform(namedInitializerExpression.Expression, context)
+                        });
+                    }
+                }
 
                 // There is no need for object creation per se, nothing should be on the right side of an assignment.
                 return Empty.Instance;
@@ -565,35 +589,49 @@ namespace Hast.Transformer.Vhdl.SubTransformers
         }
 
 
-        private void InitializeRecord(Expression expression, AstType recordAstType, ISubTransformerContext context)
+        private RecordInitializationResult InitializeRecord(Expression expression, AstType recordAstType, ISubTransformerContext context)
         {
             // Objects are mimicked with records and those don't need instantiation. However it's useful to
             // initialize all record fields to their default or initial values (otherwise if e.g. a class is
             // instantiated in a loop in the second run the old values could be accessed in VHDL).
 
-            var recordType = (Record)_typeConverter.ConvertAstType(recordAstType);
+            var record = (Record)_typeConverter.ConvertAstType(recordAstType);
+
             // This will only work if the newly created object is assigned to a variable or something else. It won't
-            // work if the newly created object is directly passed to a method for example).
+            // work if the newly created object is directly passed to a method for example.
             var recordInstanceAssignmentTarget = expression
                 .FindFirstParentOfType<AssignmentExpression>()
                 .Left;
             var recordInstanceIdentifier = recordInstanceAssignmentTarget is IdentifierExpression ?
                 recordInstanceAssignmentTarget :
                 recordInstanceAssignmentTarget.FindFirstParentOfType<IdentifierExpression>();
-            var recordInstanceReference = Transform(recordInstanceIdentifier, context);
+            var recordInstanceReference = (IDataObject)Transform(recordInstanceIdentifier, context);
 
-            foreach (var field in recordType.Fields)
+            foreach (var field in record.Fields)
             {
                 context.Scope.CurrentBlock.Add(new Assignment
                 {
                     AssignTo = new RecordFieldAccess
                     {
-                        Instance = (IDataObject)recordInstanceReference,
+                        Instance = recordInstanceReference,
                         FieldName = field.Name
                     },
                     Expression = field.DataType.DefaultValue
                 });
             }
+
+            return new RecordInitializationResult
+            {
+                Record = record,
+                RecordInstanceReference = recordInstanceReference
+            };
+        }
+
+
+        private class RecordInitializationResult
+        {
+            public Record Record { get; set; }
+            public IDataObject RecordInstanceReference { get; set; }
         }
     }
 }
