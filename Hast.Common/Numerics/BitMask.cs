06a90423e8264b2d3b1ec0493f4c2b7a4735bad6
Benedek Farkas <benedek.farkas@lombiq.com>
2017-02-05 23:35:31 +0100

    BitMask: Adding all the necessary operators and functions to be used with Unum
    --HG--
    branch : issue/HAST-90

--- Hast.Common/Numerics/BitMask.cs
+++ Hast.Common/Numerics/BitMask.cs
@@ -5,21 +5,11 @@ namespace Hast.Common.Numerics
     public struct BitMask
     {
         private const uint _32BitMSBMask = 0x80000000;
-        private const uint _uintMax = 0xFFFFFFFF;
 
         public uint Size { get; private set; }
         public uint SegmentCount { get; private set; }
         public uint[] Segments { get; private set; }
         
-
-        public BitMask(byte value)
-        {
-            Size = 8;
-            SegmentCount = 1;
-            Segments = new uint[SegmentCount];
-
-            Segments[0] = value;
-        }
 
         public BitMask(uint value)
         {
@@ -54,7 +44,16 @@ namespace Hast.Common.Numerics
             SegmentCount = size == 0 ? 0 : (size >> 5) + 1;
             Segments = new uint[SegmentCount];
 
-            if (allOne) for (int i = 0; i < SegmentCount; i++) Segments[i] = _uintMax;
+            if (allOne) for (int i = 0; i < SegmentCount; i++) Segments[i] = uint.MaxValue;
+        }
+
+        public BitMask(BitMask source)
+        {
+            Size = source.Size;
+            SegmentCount = source.SegmentCount;
+            Segments = new uint[SegmentCount];
+
+            for (int i = 0; i < SegmentCount; i++) Segments[i] = source.Segments[i];
         }
 
 
@@ -62,13 +61,113 @@ namespace Hast.Common.Numerics
         {
             if (position > mask.Size) return new BitMask(mask.Size, false);
 
-            // Integer conversion doesn't matter, because we only care about the bits, not the actual value represented.
-            int bitPosition = (int)position | 0x00000005;
+            // Integer conversion doesn't matter, because we only care about the bits,
+            // not the actual value represented, but it's needed for the bit shift to work.
+            var bitPosition = (int)position | 0x00000005;
             var segmentPosition = position >> 5;
 
             mask.Segments[segmentPosition] = (uint)1 << bitPosition;
 
             return mask;
+        }
+
+
+        public static BitMask operator +(BitMask left, uint right)
+        {
+            if (left.SegmentCount == 0) return left;
+
+            var mask = new BitMask(left);
+
+            var msbBefore = GetSegmentMSB(mask.Segments[0]);
+            mask.Segments[0] += right;
+            var msbAfter = GetSegmentMSB(mask.Segments[0]);
+
+            if (msbBefore && !msbAfter) // Carry from the first segment.
+                for (int i = 1; i < mask.SegmentCount; i++)
+                {
+                    // Does the carry bubble up through this segment?
+                    if (mask.Segments[i] == uint.MaxValue) mask.Segments[i] = 0;
+                    else
+                    {
+                        mask.Segments[i]++;
+                        break;
+                    }
+                }
+
+            return mask;
+        }
+
+        public static BitMask operator -(BitMask left, uint right)
+        {
+            if (left.SegmentCount == 0) return left;
+
+            var mask = new BitMask(left);
+
+            var msbBefore = GetSegmentMSB(mask.Segments[0]);
+            mask.Segments[0] -= right;
+            var msbAfter = GetSegmentMSB(mask.Segments[0]);
+
+            if (!msbBefore && msbAfter) // Carry from the first segment.
+                for (int i = 1; i < mask.SegmentCount; i++)
+                {
+                    // Does the carry bubble up through this segment?
+                    if (mask.Segments[i] == 0) mask.Segments[i] = uint.MaxValue;
+                    else
+                    {
+                        mask.Segments[i]--;
+                        break;
+                    }
+                }
+
+            return mask;
+        }
+
+        public static BitMask operator +(BitMask left, BitMask right)
+        {
+            if (left.Size != right.Size) return new BitMask(left.Size, false);
+
+            var mask = new BitMask(left);
+            bool carry = false, msbBefore, msbAfter;
+
+            for (int i = 0; i < mask.SegmentCount; i++)
+            {
+                msbBefore = GetSegmentMSB(mask.Segments[i]);
+
+                if (carry) mask.Segments[i]++;
+                mask.Segments[i] += right.Segments[i];
+                
+                msbAfter = GetSegmentMSB(mask.Segments[i]);
+                carry = msbBefore && !msbAfter;
+            }
+
+            return mask;
+        }
+
+        public static BitMask operator -(BitMask left, BitMask right)
+        {
+            if (left.Size != right.Size) return new BitMask(left.Size, false);
+
+            var mask = new BitMask(left);
+            bool carry = false, msbBefore, msbAfter;
+
+            for (int i = 0; i < mask.SegmentCount; i++)
+            {
+                msbBefore = GetSegmentMSB(mask.Segments[i]);
+
+                if (carry) mask.Segments[i]++;
+                mask.Segments[i] -= right.Segments[i];
+
+                msbAfter = GetSegmentMSB(mask.Segments[i]);
+                carry = !msbBefore && msbAfter;
+            }
+
+            return mask;
+        }
+
+
+        private static bool GetSegmentMSB(uint segment)
+        {
+           return segment >> 31 == 1;
         }
     }
 }
