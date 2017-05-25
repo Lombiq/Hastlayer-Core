0235e11d9af7590377fa451880bbc3580d7d54fa
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-05-25 23:44:33 +0200

    Adding transient caching to RecordComposer
    --HG--
    branch : issue/HAST-111

--- Hast.Transformer.Vhdl/SubTransformers/RecordComposer.cs
+++ Hast.Transformer.Vhdl/SubTransformers/RecordComposer.cs
@@ -5,6 +5,7 @@ using Hast.VhdlBuilder.Extensions;
 using Hast.VhdlBuilder.Representation.Declaration;
 using ICSharpCode.NRefactory.CSharp;
 using Mono.Cecil;
+using Orchard.Caching;
 
 namespace Hast.Transformer.Vhdl.SubTransformers
 {
@@ -12,11 +13,13 @@ namespace Hast.Transformer.Vhdl.SubTransformers
     {
         // Needs Lazy because unfortunately TypeConverter and RecordComposer depend on each other.
         private readonly Lazy<ITypeConverter> _typeConverterLazy;
+        private readonly ICacheManager _cacheManager;
 
 
-        public RecordComposer(Lazy<ITypeConverter> typeConverterLazy)
+        public RecordComposer(Lazy<ITypeConverter> typeConverterLazy, ICacheManager cacheManager)
         {
             _typeConverterLazy = typeConverterLazy;
+            _cacheManager = cacheManager;
         }
 
 
@@ -27,36 +30,41 @@ namespace Hast.Transformer.Vhdl.SubTransformers
 
         public Record CreateRecordFromType(TypeDeclaration typeDeclaration, ITypeDeclarationLookupTable typeDeclarationLookupTable)
         {
-            // Process only those fields that aren't backing fields of auto-properties (since those properties are 
-            // handled as properties) nor const fields (those are inserted as literals by the compiler where they're
-            // used).
-            var recordFields = typeDeclaration.Members
-                .Where(member => 
-                    member is PropertyDeclaration || 
-                    member.Is<FieldDeclaration>(field => !field.GetFullName().IsBackingFieldName()))
-                .Select(member =>
-                {
-                    var name = member.Name;
-
-                    if (member is FieldDeclaration)
-                    {
-                        name = ((FieldDeclaration)member).Variables.Single().Name;
-                    }
-
-                    return new RecordField
-                    {
-                        DataType = _typeConverterLazy.Value.ConvertAstType(member.ReturnType, typeDeclarationLookupTable),
-                        Name = name.ToExtendedVhdlId()
-                    };
-
-                })
-                .ToList();
-
-            return new Record
+            // Using transient caching because when processing an assembly all references to a class or struct will 
+            // result in the record being composed.
+            return _cacheManager.Get("ComposedRecord." + typeDeclaration.GetFullName(), true, context =>
             {
-                Name = typeDeclaration.GetFullName().ToExtendedVhdlId(),
-                Fields = recordFields
-            };
+                // Process only those fields that aren't backing fields of auto-properties (since those properties are 
+                // handled as properties) nor const fields (those are inserted as literals by the compiler where they're
+                // used).
+                var recordFields = typeDeclaration.Members
+                    .Where(member =>
+                        member is PropertyDeclaration ||
+                        member.Is<FieldDeclaration>(field => !field.GetFullName().IsBackingFieldName()))
+                    .Select(member =>
+                    {
+                        var name = member.Name;
+
+                        if (member is FieldDeclaration)
+                        {
+                            name = ((FieldDeclaration)member).Variables.Single().Name;
+                        }
+
+                        return new RecordField
+                        {
+                            DataType = _typeConverterLazy.Value.ConvertAstType(member.ReturnType, typeDeclarationLookupTable),
+                            Name = name.ToExtendedVhdlId()
+                        };
+
+                    })
+                    .ToList();
+
+                return new Record
+                {
+                    Name = typeDeclaration.GetFullName().ToExtendedVhdlId(),
+                    Fields = recordFields
+                };
+            });
         }
     }
 }
