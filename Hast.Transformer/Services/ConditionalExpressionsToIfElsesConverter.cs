0e21c64f90e3a4270c6271281f7914138b52f4da
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-05-27 20:13:44 +0200

    Improving conditional expression support: now even if the result is not assigned to a variable the expression is supported
    --HG--
    branch : issue/HAST-111

--- Hast.Transformer/Services/ConditionalExpressionsToIfElsesConverter.cs
+++ Hast.Transformer/Services/ConditionalExpressionsToIfElsesConverter.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hast.Transformer.Helpers;
+using ICSharpCode.Decompiler.Ast;
+using ICSharpCode.Decompiler.ILAst;
 using ICSharpCode.NRefactory.CSharp;
 
 namespace Hast.Transformer.Services
@@ -23,11 +26,34 @@ namespace Hast.Transformer.Services
 
                 var assignment = conditionalExpression.Parent as AssignmentExpression;
 
-                // Only simple "variable = condition ? value1 : value2" expressions are supported now.
-                if (assignment == null || 
+                // Simple "variable = condition ? value1 : value2" expressions are easily handled but ones not in this
+                // form need some further work.
+                if (assignment == null ||
                     !(assignment.Left is IdentifierExpression) ||
                     !(assignment.Parent is ExpressionStatement))
-                    return;
+                {
+                    var variableName = "conditational" + conditionalExpression.GetHashCode().ToString();
+                    var variableTypeReference = conditionalExpression.GetActualTypeReference();
+
+                    // First creating a variable for the result.
+                    var variableDeclaration = 
+                        new VariableDeclarationStatement(AstBuilder.ConvertType(variableTypeReference), variableName);
+                    AstInsertionHelper.InsertStatementBefore(
+                        conditionalExpression.FindFirstParentOfType<Statement>(),
+                        variableDeclaration);
+
+                    // Then moving the conditational expression so its result is assigned to the variable.
+                    var variableIdentifier = new IdentifierExpression(variableName);
+                    variableIdentifier.AddAnnotation(new ILVariable { Name = variableName, Type = variableTypeReference });
+                    var newConditionalExpression = (ConditionalExpression)conditionalExpression.Clone();
+                    assignment = new AssignmentExpression(variableIdentifier, newConditionalExpression);
+                    assignment.AddAnnotation(conditionalExpression.Annotation<TypeInformation>());
+                    AstInsertionHelper.InsertStatementAfter(variableDeclaration, new ExpressionStatement(assignment));
+
+                    // And finally swapping out the original expression with the variable reference.
+                    conditionalExpression.ReplaceWith(variableIdentifier.Clone());
+                    conditionalExpression = newConditionalExpression;
+                }
 
                 var trueAssignment = (AssignmentExpression)assignment.Clone();
                 trueAssignment.Right = conditionalExpression.TrueExpression.Clone();
