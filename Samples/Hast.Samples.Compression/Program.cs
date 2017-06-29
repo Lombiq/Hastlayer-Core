abef74a689b67338ca4fb08ac105a816c6568cad
Márk Bartha <barthamark@gmail.com>
2017-06-29 15:20:15 +0200

    Adding a simple LZMA compression project using the SevenZip SDK
    --HG--
    branch : issue/HAST-88

---
+++ Samples/Hast.Samples.Compression/Program.cs
@@ -0,0 +1,30 @@
+﻿using Hast.Samples.Compression.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Hast.Samples.Compression
+{
+    class Program
+    {
+        public const string InputFilePath = @"TestFiles\SmallTextFile.txt";
+        public const string OutputFilePath = @"TestFiles\SmallTextFile-compressed.txt.lzma";
+
+
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                using (var hastlayer = Xilinx.HastlayerFactory.Create())
+                {
+                    Console.WriteLine($"Compressing {InputFilePath}...");
+
+                    await LzmaCompressionService.CompressFile(InputFilePath, OutputFilePath);
+
+                    Console.WriteLine("Compression finished.");
+                }
+            }).Wait();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Samples/Hast.Samples.Compression/Services/LzmaCompressionService.cs
@@ -0,0 +1,73 @@
+﻿using Hast.Layer;
+using SevenZip;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace Hast.Samples.Compression.Services
+{
+    public static class LzmaCompressionService
+    {
+        public static async Task CompressFile(string inputFilePath, string outputFilePath)
+        {
+            var inputFile = new FileInfo(inputFilePath);
+
+            if (!inputFile.Exists) throw new HastlayerException("Input file doesn't exist.");
+
+            outputFilePath = string.IsNullOrEmpty(outputFilePath) ? inputFile.Name + ".lzma" : outputFilePath;
+            if (File.Exists(outputFilePath)) File.Delete(outputFilePath);
+
+            using (var inputFileStream = inputFile.OpenRead())
+            using (var outputFileStream = File.Create(outputFilePath))
+            {
+                string mf = "bt4";
+                var dictionary = 1 << 23;
+                var posStateBits = 2;
+                int litContextBits = 3; // for normal files
+                                        // UInt32 litContextBits = 0; // for 32-bit data
+                int litPosBits = 0;
+                // UInt32 litPosBits = 2; // for 32-bit data
+                int algorithm = 2;
+                int numFastBytes = 128;
+                var stdInMode = false;
+                bool eos = stdInMode;
+
+                CoderPropID[] propIDs =
+                {
+                    CoderPropID.DictionarySize,
+                    CoderPropID.PosStateBits,
+                    CoderPropID.LitContextBits,
+                    CoderPropID.LitPosBits,
+                    CoderPropID.Algorithm,
+                    CoderPropID.NumFastBytes,
+                    CoderPropID.MatchFinder,
+                    CoderPropID.EndMarker
+                };
+                object[] properties =
+                {
+                    (int)(dictionary),
+                    (int)(posStateBits),
+                    (int)(litContextBits),
+                    (int)(litPosBits),
+                    (int)(algorithm),
+                    (int)(numFastBytes),
+                    mf,
+                    eos
+                };
+                var encoder = new SevenZip.Compression.LZMA.Encoder();
+                encoder.SetCoderProperties(propIDs, properties);
+                encoder.WriteCoderProperties(outputFileStream);
+
+                long fileSize;
+                if (eos || stdInMode)
+                    fileSize = -1;
+                else
+                    fileSize = inputFileStream.Length;
+                for (int i = 0; i < 8; i++)
+                    outputFileStream.WriteByte((byte)(fileSize >> (8 * i)));
+
+                encoder.Code(inputFileStream, outputFileStream, -1, -1, null);
+            }
+        }
+    }
+}
