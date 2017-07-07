d9d34ebafad961b2aa74ceaf051024b2834fd872
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-07-07 15:44:24 +0200

    Fixing device driver handling in tests
    --HG--
    branch : issue/HAST-118

--- Hast.Transformer.Vhdl/Hast.Transformer.Vhdl.Tests/VhdlTransformingTestFixtureBase.cs
+++ Hast.Transformer.Vhdl/Hast.Transformer.Vhdl.Tests/VhdlTransformingTestFixtureBase.cs
@@ -4,6 +4,10 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Autofac;
 using Hast.Common.Configuration;
+using Hast.Common.Models;
+using Hast.Synthesis;
+using Hast.Synthesis.Models;
+using Hast.Synthesis.Services;
 using Hast.Transformer.Models;
 using Hast.Transformer.Vhdl.Models;
 using Hast.Transformer.Vhdl.Tests.IntegrationTestingServices;
@@ -20,6 +24,7 @@ namespace Hast.Transformer.Vhdl.Tests
             _shellRegistrationBuilder = builder =>
             {
                 builder.RegisterInstance(new StubMemberSuitabilityChecker()).As<IMemberSuitabilityChecker>();
+                builder.RegisterInstance(new StubDeviceDriverSelector()).As<IDeviceDriverSelector>();
             };
         }
 
@@ -42,6 +47,38 @@ namespace Hast.Transformer.Vhdl.Tests
                 ITypeDeclarationLookupTable typeDeclarationLookupTable) => 
                 (member.HasModifier(Modifiers.Public) && member.FindFirstParentTypeDeclaration().HasModifier(Modifiers.Public)) ||
                 member.Modifiers == Modifiers.None;
+        }
+
+        private class StubDeviceDriverSelector : IDeviceDriverSelector
+        {
+            public IDeviceDriver GetDriver(string deviceName) =>new StubDeviceDriver();
+
+            public IEnumerable<IDeviceManifest> GetSupporteDevices()
+            {
+                throw new NotImplementedException();
+            }
+
+
+            private class StubDeviceDriver : IDeviceDriver
+            {
+                public IDeviceManifest DeviceManifest
+                {
+                    get
+                    {
+                        return new DeviceManifest
+                        {
+                            Name = "Nexys4 DDR",
+                            ClockFrequencyHz = 100000000, // 100 Mhz
+                            SupportedCommunicationChannelNames = new[] { "Serial", "Ethernet" },
+                            AvailableMemoryBytes = 115343360 // 110MB
+                        };
+                    }
+                }
+
+                public decimal GetClockCyclesNeededForBinaryOperation(BinaryOperatorExpression expression, int operandSizeBits, bool isSigned) => 0;
+
+                public decimal GetClockCyclesNeededForUnaryOperation(UnaryOperatorExpression expression, int operandSizeBits, bool isSigned) => 0;
+            }
         }
     }
 }
