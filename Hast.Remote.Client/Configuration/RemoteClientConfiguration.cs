78abcccae51de741a6040e1bca6b2f8e6617a681
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-07-12 13:43:20 +0200

    Using the production Hastlayer API endpoint URL as the default in RemoteClientConfiguration
    --HG--
    branch : issue/HAST-118

--- Hast.Remote.Client/Configuration/RemoteClientConfiguration.cs
+++ Hast.Remote.Client/Configuration/RemoteClientConfiguration.cs
@@ -8,7 +8,7 @@ namespace Hast.Remote.Configuration
 {
     public class RemoteClientConfiguration
     {
-        public Uri EndpointBaseUri { get; set; } = new Uri("http://hastlayer.com.127-0-0-1.org.uk/api/Hastlayer.Frontend");
+        public Uri EndpointBaseUri { get; set; } = new Uri("https://hastlayer.com/api/Hastlayer.Frontend");
         public string AppId { get; set; }
         public string AppSecret { get; set; }
     }
