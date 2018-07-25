c61c1496d06af829a521f5a4ddabdc05b71f0f8b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2018-07-25 18:44:11 +0200

    Backed out changeset: 51da14f1aef3
    --HG--
    branch : issue/HAST-131

--- Hast.Transformer.Vhdl/SubTransformers/ArrayTypesCreator.cs
+++ Hast.Transformer.Vhdl/SubTransformers/ArrayTypesCreator.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using Hast.Transformer.Helpers;
-using Hast.Transformer.Models;
 using Hast.Transformer.Vhdl.Helpers;
 using Hast.Transformer.Vhdl.Models;
 using Hast.VhdlBuilder.Representation.Declaration;
@@ -35,7 +34,6 @@ namespace Hast.Transformer.Vhdl.SubTransformers
             private readonly ITypeConverter _typeConverter;
             private readonly Dictionary<string, ArrayType> _arrayDeclarations;
             private readonly IVhdlTransformationContext _context;
-            private readonly HashSet<string> _simpleMemoryArrayTypeNames = new HashSet<string>();
 
 
             public ArrayCreationCheckingVisitor(
@@ -46,14 +44,6 @@ namespace Hast.Transformer.Vhdl.SubTransformers
                 _typeConverter = typeConverter;
                 _arrayDeclarations = arrayDeclarations;
                 _context = context;
-
-                // Excluding array types already added by the SimpleMemory package.
-                if (context.GetTransformerConfiguration().UseSimpleMemory)
-                {
-                    _simpleMemoryArrayTypeNames.Add(ArrayHelper.CreateArrayTypeName(KnownDataTypes.Int32));
-                    _simpleMemoryArrayTypeNames.Add(ArrayHelper.CreateArrayTypeName(KnownDataTypes.UInt32));
-                    _simpleMemoryArrayTypeNames.Add(ArrayHelper.CreateArrayTypeName(KnownDataTypes.Boolean));
-                }
             }
 
 
@@ -82,7 +72,7 @@ namespace Hast.Transformer.Vhdl.SubTransformers
 
                 var typeName = ArrayHelper.CreateArrayTypeName(elementType);
 
-                if (_arrayDeclarations.ContainsKey(typeName) || _simpleMemoryArrayTypeNames.Contains(typeName)) return;
+                if (_arrayDeclarations.ContainsKey(typeName)) return;
 
                 _arrayDeclarations[typeName] = new ArrayType
                 {
