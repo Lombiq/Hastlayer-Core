f04efdb2635dbb945cb1b176b6cd4f5ee9416b34
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-10-18 21:54:26 +0200

    Code styling
    --HG--
    branch : issue/HAST-131

--- Hast.Transformer.Vhdl/ArchitectureComponents/ArchitectureComponentNameHelper.cs
+++ Hast.Transformer.Vhdl/ArchitectureComponents/ArchitectureComponentNameHelper.cs
@@ -11,44 +11,28 @@ namespace Hast.Transformer.Vhdl.ArchitectureComponents
 
     public static class ArchitectureComponentNameHelper
     {
-        public static string CreateParameterSignalName(string componentName, string parameterName, ParameterFlowDirection direction)
-        {
-            return CreatePrefixedSegmentedObjectName(componentName, parameterName, NameSuffixes.Parameter, direction.ToString());
-        }
+        public static string CreateParameterSignalName(string componentName, string parameterName, ParameterFlowDirection direction) =>
+            CreatePrefixedSegmentedObjectName(componentName, parameterName, NameSuffixes.Parameter, direction.ToString());
 
-        public static string CreateReturnSignalName(string componentName)
-        {
-            return CreatePrefixedObjectName(componentName, NameSuffixes.Return);
-        }
+        public static string CreateReturnSignalName(string componentName) =>
+            CreatePrefixedObjectName(componentName, NameSuffixes.Return);
 
-        public static string CreateStartedSignalName(string componentName)
-        {
-            return CreatePrefixedObjectName(componentName, NameSuffixes.Started);
-        }
+        public static string CreateStartedSignalName(string componentName) =>
+            CreatePrefixedObjectName(componentName, NameSuffixes.Started);
 
-        public static string CreateFinishedSignalName(string componentName)
-        {
-            return CreatePrefixedObjectName(componentName, NameSuffixes.Finished);
-        }
+        public static string CreateFinishedSignalName(string componentName) =>
+            CreatePrefixedObjectName(componentName, NameSuffixes.Finished);
 
-        public static string CreateIndexedComponentName(string componentName, int index)
-        {
-            return componentName + "." + index;
-        }
+        public static string CreateIndexedComponentName(string componentName, int index) =>
+            componentName + "." + index;
 
-        public static string CreatePrefixedSegmentedObjectName(string componentName, params string[] segments)
-        {
-            return CreatePrefixedObjectName(componentName, string.Join(".", segments));
-        }
+        public static string CreatePrefixedSegmentedObjectName(string componentName, params string[] segments) =>
+            CreatePrefixedObjectName(componentName, string.Join(".", segments));
 
-        public static string CreatePrefixedObjectName(string componentName, string name)
-        {
-            return CreatePrefixedExtendedVhdlId(componentName, "." + name);
-        }
+        public static string CreatePrefixedObjectName(string componentName, string name) =>
+            CreatePrefixedExtendedVhdlId(componentName, "." + name);
 
-        public static string CreatePrefixedExtendedVhdlId(string componentName, string id)
-        {
-            return (componentName + id).ToExtendedVhdlId();
-        }
+        public static string CreatePrefixedExtendedVhdlId(string componentName, string id) =>
+            (componentName + id).ToExtendedVhdlId();
     }
 }
