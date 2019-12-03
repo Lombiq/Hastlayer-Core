cbf93bf4ae4c67b71c7d58d2ab9a0c164b080434
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-12-03 23:55:32 +0100

    Code styling
    --HG--
    branch : issue/HAST-141

--- Hast.Transformer/Extensions/SyntaxTreeExtensions.cs
+++ Hast.Transformer/Extensions/SyntaxTreeExtensions.cs
@@ -5,9 +5,7 @@ namespace ICSharpCode.Decompiler.CSharp
 {
     public static class SyntaxTreeExtensions
     {
-        public static IEnumerable<TypeDeclaration> GetAllTypeDeclarations(this SyntaxTree syntaxTree)
-        {
-            return syntaxTree.GetTypes(true).Where(type => type is TypeDeclaration).Cast<TypeDeclaration>();
-        }
+        public static IEnumerable<TypeDeclaration> GetAllTypeDeclarations(this SyntaxTree syntaxTree) =>
+            syntaxTree.GetTypes(true).Where(type => type is TypeDeclaration).Cast<TypeDeclaration>();
     }
 }
