bad520f566515d119143d050b98959c15fb5586e
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-12-23 22:20:22 +0100

    Removing unused extension method
    --HG--
    branch : issue/HAST-141

--- Hast.Transformer/Extensions/InvocationExpressionExtensions.cs
+++ Hast.Transformer/Extensions/InvocationExpressionExtensions.cs
@@ -4,22 +4,6 @@ namespace ICSharpCode.Decompiler.CSharp.Syntax
 {
     public static class InvocationExpressionExtensions
     {
-        /// <summary>
-        /// Retrieves the return type of the method that was invoked in an invocation expression.
-        /// </summary>
-        public static TypeReference GetReturnTypeReference(this InvocationExpression expression)
-        {
-            // Looking up the type information that will tell us what the return type of the invocation is. 
-            // This might be some nodes up if e.g. there is an immediate cast expression.
-            AstNode currentNode = expression;
-            while (currentNode.Annotation<TypeInformation>() == null)
-            {
-                currentNode = currentNode.Parent;
-            }
-
-            return currentNode.GetActualType();
-        }
-
         public static string GetTargetMemberFullName(this InvocationExpression expression) =>
             expression.GetReferencedMemberFullName();
 
