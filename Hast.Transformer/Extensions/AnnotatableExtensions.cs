e7d67fdc5cec131247307bd7ac21834ebe063032
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-12-23 02:53:15 +0100

    Disabling GetActualType() until we can actually run the app
    --HG--
    branch : issue/HAST-141

--- Hast.Transformer/Extensions/AnnotatableExtensions.cs
+++ Hast.Transformer/Extensions/AnnotatableExtensions.cs
@@ -1,7 +1,6 @@
-﻿using Hast.Transformer.Helpers;
-using ICSharpCode.Decompiler.IL;
-using ICSharpCode.Decompiler.TypeSystem;
+﻿using ICSharpCode.Decompiler.TypeSystem;
 using Mono.Cecil;
+using System;
 
 namespace ICSharpCode.Decompiler.CSharp.Syntax
 {
@@ -9,52 +8,53 @@ namespace ICSharpCode.Decompiler.CSharp.Syntax
     {
         public static IType GetActualType(this IAnnotatable annotable, bool getExpectedType = false)
         {
-            var typeInformation = annotable.Annotation<TypeInformation>();
-            if (typeInformation != null)
-            {
-                if (getExpectedType) return typeInformation.ExpectedType;
-                else return typeInformation.InferredType ?? typeInformation.ExpectedType;
-            }
+            throw new NotImplementedException();
+            //var typeInformation = annotable.Annotation<TypeInformation>();
+            //if (typeInformation != null)
+            //{
+            //    if (getExpectedType) return typeInformation.ExpectedType;
+            //    else return typeInformation.InferredType ?? typeInformation.ExpectedType;
+            //}
 
-            var typeReference = annotable.Annotation<TypeReference>();
-            if (typeReference != null) return typeReference;
+            //var typeReference = annotable.Annotation<TypeReference>();
+            //if (typeReference != null) return typeReference;
 
-            var ilVariable = annotable.Annotation<ILVariable>();
-            if (ilVariable != null) return ilVariable.Type;
+            //var ilVariable = annotable.Annotation<ILVariable>();
+            //if (ilVariable != null) return ilVariable.Type;
 
-            var fieldReference = annotable.Annotation<FieldReference>();
-            if (fieldReference != null) return fieldReference.FieldType;
+            //var fieldReference = annotable.Annotation<FieldReference>();
+            //if (fieldReference != null) return fieldReference.FieldType;
 
-            var propertyReference = annotable.Annotation<PropertyReference>();
-            if (propertyReference != null) return propertyReference.PropertyType;
+            //var propertyReference = annotable.Annotation<PropertyReference>();
+            //if (propertyReference != null) return propertyReference.PropertyType;
 
-            var methodReference = annotable.Annotation<MethodReference>();
-            if (methodReference != null) return methodReference.ReturnType;
+            //var methodReference = annotable.Annotation<MethodReference>();
+            //if (methodReference != null) return methodReference.ReturnType;
 
-            var parameterReference = annotable.Annotation<ParameterReference>();
-            if (parameterReference != null) return parameterReference.ParameterType;
+            //var parameterReference = annotable.Annotation<ParameterReference>();
+            //if (parameterReference != null) return parameterReference.ParameterType;
 
-            if (annotable is IndexerExpression indexerExpression)
-            {
-                return indexerExpression.Target.GetActualType()?.GetElementType();
-            }
+            //if (annotable is IndexerExpression indexerExpression)
+            //{
+            //    return indexerExpression.Target.GetActualType()?.GetElementType();
+            //}
 
-            if (annotable is AssignmentExpression assignmentExpression)
-            {
-                return assignmentExpression.Left.GetActualType();
-            }
+            //if (annotable is AssignmentExpression assignmentExpression)
+            //{
+            //    return assignmentExpression.Left.GetActualType();
+            //}
 
-            if (annotable is UnaryOperatorExpression unaryOperatorExpression)
-            {
-                return unaryOperatorExpression.Expression.GetActualType();
-            }
+            //if (annotable is UnaryOperatorExpression unaryOperatorExpression)
+            //{
+            //    return unaryOperatorExpression.Expression.GetActualType();
+            //}
 
-            if (annotable is PrimitiveExpression primitiveExpression)
-            {
-                return TypeHelper.CreatePrimitiveTypeReference(primitiveExpression.Value.GetType().Name);
-            }
+            //if (annotable is PrimitiveExpression primitiveExpression)
+            //{
+            //    return TypeHelper.CreatePrimitiveTypeReference(primitiveExpression.Value.GetType().Name);
+            //}
 
-            return null;
+            //return null;
         }
 
         public static void CopyAnnotationsTo(this IAnnotatable annotable, IAnnotatable toNode)
