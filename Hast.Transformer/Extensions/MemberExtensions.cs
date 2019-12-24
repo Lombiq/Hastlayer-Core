b044342288830f4d2757beb90fb45f5163bbe354
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-12-25 00:48:07 +0100

    Fixing InstanceMethodsToStaticConverter
    --HG--
    branch : issue/HAST-141

--- Hast.Transformer/Extensions/MemberExtensions.cs
+++ Hast.Transformer/Extensions/MemberExtensions.cs
@@ -6,11 +6,23 @@ namespace ICSharpCode.Decompiler.TypeSystem
     {
         public static string GetFullName(this IMember member)
         {
-            var name = $"{member.ReturnType.FullName} {member.DeclaringType.FullName}::{member.Name}";
+            var name = member.ReturnType.FullName + " ";
+
+            var declaringTypeNames = string.Empty;
+            var currentType = member.DeclaringType;
+            while (currentType.DeclaringType != null)
+            {
+                declaringTypeNames = "/" + currentType.Name + declaringTypeNames;
+                currentType = currentType.DeclaringType;
+            }
+
+            name += $"{currentType.FullName}{declaringTypeNames}::{member.Name}";
+
             if (member is IMethod method)
             {
                 name += $"({string.Join(", ", method.Parameters.Select(parameter => parameter.Type.FullName))})";
             }
+
             return name;
         }
     }
