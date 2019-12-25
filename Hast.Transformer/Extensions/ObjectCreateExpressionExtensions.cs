816cfd59c4fac013dab7fde39a7a3d7ce8e3ef98
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2019-12-25 19:58:35 +0100

    Fixing that constructors were cleaned up
    --HG--
    branch : issue/HAST-141

--- Hast.Transformer/Extensions/ObjectCreateExpressionExtensions.cs
+++ Hast.Transformer/Extensions/ObjectCreateExpressionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Hast.Transformer.Helpers;
 using Hast.Transformer.Models;
 using ICSharpCode.Decompiler.Semantics;
+using ICSharpCode.Decompiler.TypeSystem;
 using System.Linq;
 
 namespace ICSharpCode.Decompiler.CSharp.Syntax
@@ -8,7 +9,7 @@ namespace ICSharpCode.Decompiler.CSharp.Syntax
     public static class ObjectCreateExpressionExtensions
     {
         public static string GetConstructorFullName(this ObjectCreateExpression objectCreateExpression) =>
-            objectCreateExpression.GetResolveResult<InvocationResolveResult>()?.Member.FullName;
+            objectCreateExpression.GetResolveResult<InvocationResolveResult>()?.Member.GetFullName();
 
         public static EntityDeclaration FindConstructorDeclaration(
             this ObjectCreateExpression objectCreateExpression,
