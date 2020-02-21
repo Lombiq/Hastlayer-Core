795c2825463a485cc8964922e02d76ec65fde489
David El-Saig <david.el-saig@lombiq.com>
2020-02-21 14:17:45 +0100

    Fixed issue where loading in all Nexys drivers would cause them to overwrite each other's names as the manifest name is static.
    --HG--
    branch : issue/HAST-156

--- Hast.Xilinx/Nexys4DdrDriver.cs
+++ Hast.Xilinx/Nexys4DdrDriver.cs
@@ -5,7 +5,7 @@ namespace Hast.Xilinx
 {
     public class Nexys4DdrDriver : NexysDriverBase
     {
-        static Nexys4DdrDriver() => DeviceNameInternal = Nexys4DdrManifestProvider.DeviceName;
+        static Nexys4DdrDriver() => DeviceNameInternals[nameof(Nexys4DdrDriver)] = Nexys4DdrManifestProvider.DeviceName;
 
         public Nexys4DdrDriver(ITimingReportParser timingReportParser) : base(timingReportParser)
         {
--- Hast.Xilinx/NexysA7Driver.cs
+++ Hast.Xilinx/NexysA7Driver.cs
@@ -5,7 +5,7 @@ namespace Hast.Xilinx
 {
     public class NexysA7Driver : NexysDriverBase
     {
-        static NexysA7Driver() => DeviceNameInternal = NexysA7ManifestProvider.DeviceName;
+        static NexysA7Driver() => DeviceNameInternals[nameof(NexysA7Driver)] = NexysA7ManifestProvider.DeviceName;
 
         public NexysA7Driver(ITimingReportParser timingReportParser) : base(timingReportParser)
         {
