3850df16e9916f851120f12a7d2a68a8e5640377
David El-Saig <david.el-saig@lombiq.com>
2020-02-26 00:52:56 +0100

    Added an extension method to easily override loaded service with its mock.
    --HG--
    branch : issue/HAST-156

---
+++ Hast.TestBase/Extensions/MoqExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moq
+{
+    public static class MoqExtensions
+    {
+        public static void ForceMock<T>(this Mock<T> mock, IServiceCollection services) where T : class
+        {
+            services.RemoveImplementations<T>();
+            services.AddSingleton(mock.Object);
+        }
+    }
+}
