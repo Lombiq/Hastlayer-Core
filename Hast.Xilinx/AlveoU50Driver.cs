41acab1de0a63d2922664b1fa4f2241c736edf65
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-07-21 00:47:45 +0200

    Adding U50 driver skeleton

---
+++ Hast.Xilinx/AlveoU50Driver.cs
@@ -0,0 +1,44 @@
+using Hast.Synthesis;
+using Hast.Synthesis.Helpers;
+using Hast.Synthesis.Models;
+using Hast.Synthesis.Services;
+using Hast.Xilinx.Abstractions.ManifestProviders;
+using ICSharpCode.Decompiler.CSharp.Syntax;
+
+namespace Hast.Xilinx
+{
+    public class AlveoU50Driver : AlveoU50ManifestProvider, IDeviceDriver
+    {
+        private readonly ITimingReportParser _timingReportParser;
+
+        private readonly object _timingReportParserLock = new object();
+
+        private ITimingReport _timingReport;
+        private ITimingReport TimingReport
+        {
+            get
+            {
+                lock (_timingReportParserLock)
+                {
+                    if (_timingReport == null)
+                    {
+                        var timingReport = "";
+                        _timingReport = _timingReportParser.Parse(timingReport);
+                    }
+
+                    return _timingReport;
+                }
+            }
+        }
+
+
+        public AlveoU50Driver(ITimingReportParser timingReportParser) => _timingReportParser = timingReportParser;
+
+
+        public decimal GetClockCyclesNeededForBinaryOperation(BinaryOperatorExpression expression, int operandSizeBits, bool isSigned) =>
+            DeviceDriverHelper.ComputeClockCyclesForBinaryOperation(DeviceManifest, TimingReport, expression, operandSizeBits, isSigned);
+
+        public decimal GetClockCyclesNeededForUnaryOperation(UnaryOperatorExpression expression, int operandSizeBits, bool isSigned) =>
+            DeviceDriverHelper.ComputeClockCyclesForUnaryOperation(DeviceManifest, TimingReport, expression, operandSizeBits, isSigned);
+    }
+}
