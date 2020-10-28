552e92870ae7a5f06b24bcaa8e2129d085e990d6
Dávid El-Saig <david.el-saig@lombiq.com>
2020-10-28 20:12:47 +0100

    Fixed docs.

--- Hast.Transformer/Services/IReadonlyToConstConverter.cs
+++ Hast.Transformer/Services/IReadonlyToConstConverter.cs
@@ -10,6 +10,26 @@ namespace Hast.Transformer.Services
     /// substitution. Only applies to fields that have the <see cref="ReplaceableAttribute"/>.
     /// </summary>
     /// <example>
+    /// If you have this code:
+    /// <code>
+    ///     public class ParallelAlgorithm
+    ///     {
+    ///         [Replaceable(nameof(ParallelAlgorithm) + "." + nameof(MaxDegreeOfParallelism))]
+    ///         public readonly int MaxDegreeOfParallelism = 260;
+    ///         // ..
+    ///         public virtual void Run(SimpleMemory memory)
+    ///         {
+    ///             var input = memory.ReadInt32(Run_InputInt32Index);
+    ///             var tasks = new Task&lt;int&gt;[MaxDegreeOfParallelism];
+    /// </code>
+    /// The last line becomes by default:
+    /// <code>
+    ///             var tasks = new Task&lt;int&gt;[260];
+    /// </code>
+    /// But with substitution of 123:
+    /// <code>
+    ///             var tasks = new Task&lt;int&gt;[123];
+    /// </code>
     /// </example>
     public interface IReadonlyToConstConverter : IDependency
     {
@@ -17,7 +37,7 @@ namespace Hast.Transformer.Services
         /// Finds and replaces any <see langword="public"/> <see langword="static"/> <see langword="readonly"/> fields
         /// in <paramref name="syntaxTree"/> that have <see cref="ReplaceableAttribute"/> with their assigned literal
         /// value. If <see cref="IHardwareGenerationConfiguration.CustomConfiguration"/> in
-        /// <paramref name="configuration"/> has a <c>Replaceable</c> of type <c>IDictionary&gt;string, string&lt;</c>
+        /// <paramref name="configuration"/> has a <c>Replaceable</c> of type <c>IDictionary&lt;string, string&gt;</c>
         /// with the matching value identified by the attribute, then it is used instead of the assigned value. Read
         /// "Using dynamic constants" in <c>Docs/DevelopingHastlayer.md</c> for usage information.
         /// </summary>
