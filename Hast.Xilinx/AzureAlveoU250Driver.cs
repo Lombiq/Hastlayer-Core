66b0ff7612b84e744642eb3ee2f35bcf198c8c7b
Dávid El-Saig <david.el-saig@lombiq.com>
2020-12-26 05:16:09 +0100

    Added Azure U250 driver.

---
+++ Hast.Xilinx/AzureAlveoU250Driver.cs
@@ -0,0 +1,29 @@
+using Hast.Synthesis;
+using Hast.Synthesis.Models;
+using Hast.Synthesis.Services;
+using Hast.Xilinx.Abstractions.ManifestProviders;
+
+namespace Hast.Xilinx
+{
+    public class AzureAlveoU250Driver : AzureAlveoU250ManifestProvider, IDeviceDriver
+    {
+        private readonly ITimingReportParser _timingReportParser;
+        private readonly object _timingReportParserLock = new object();
+
+        private ITimingReport _timingReport;
+        public ITimingReport TimingReport
+        {
+            get
+            {
+                lock (_timingReportParserLock)
+                {
+                    _timingReport ??= _timingReportParser.Parse(ResourceHelper.GetTimingReport(nameof(AlveoU250Driver)));
+
+                    return _timingReport;
+                }
+            }
+        }
+
+        public AzureAlveoU250Driver(ITimingReportParser timingReportParser) => _timingReportParser = timingReportParser;
+    }
+}
