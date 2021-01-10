7572c0c5b6c7f5d758c8a9fbc9f7f253fa248110
Dávid El-Saig <david.el-saig@lombiq.com>
2021-01-11 00:20:13 +0100

    Added manifest providers.

---
+++ Hast.Xilinx/ResourceHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace Hast.Xilinx
+{
+    public static class ResourceHelper
+    {
+        public static string GetTimingReport(string csvName)
+        {
+            var provider = new EmbeddedFileProvider(typeof(AlveoU250Driver).Assembly);
+            using var stream = provider.GetFileInfo($"TimingReports.{csvName}.csv").CreateReadStream();
+            using var streamReader = new StreamReader(stream);
+            return streamReader.ReadToEnd();
+        }
+    }
+}
