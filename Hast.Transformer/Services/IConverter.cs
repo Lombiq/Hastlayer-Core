751899cde77c950577e784919b1a6e1f98c20f20
Dávid El-Saig <david.el-saig@lombiq.com>
2021-06-10 00:25:20 +0200

    Add IConverter. TODO: Use this as common interface for the services used by DefaultTransformer.

---
+++ Hast.Transformer/Services/IConverter.cs
@@ -0,0 +1,18 @@
+﻿using Hast.Common.Interfaces;
+using Hast.Layer;
+using Hast.Transformer.Abstractions;
+using ICSharpCode.Decompiler.CSharp.Syntax;
+
+namespace Hast.Transformer.Services
+{
+    /// <summary>
+    /// A common interface for any converters utilized by <see cref="ITransformer"/>.
+    /// </summary>
+    public interface IConverter : IDependency
+    {
+        /// <summary>
+        /// Performs a conversion operation by altering the <paramref name="syntaxTree"/>.
+        /// </summary>
+        void Convert(SyntaxTree syntaxTree, IHardwareGenerationConfiguration configuration);
+    }
+}
