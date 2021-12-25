b2e8aec616e034d58817694f7dae49ae1051ae9f
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-26 00:34:01 +0100

    VHDL verification: only due to change in the comment template.

--- Hast.Transformer.Vhdl/Hast.Transformer.Vhdl.Tests/VerificationTests/VerificationSources/CatapultSamplesVerificationTests.Posit32FusedSampleMatchesApproved.approved.vhdl
+++ Hast.Transformer.Vhdl/Hast.Transformer.Vhdl.Tests/VerificationTests/VerificationSources/CatapultSamplesVerificationTests.Posit32FusedSampleMatchesApproved.approved.vhdl
@@ -154,15 +154,24 @@ entity Hast_IP is
     --             the Started port above on how it is used.
     -- * Reset: Synchronous reset.
     -- * Clock: The main clock.
-    -- * DataIn: Data read out from the memory (usually on-board DDR RAM, but depends on the framework) should be assigned to this port by the framework. The width of this port is always 32b, independent of the hardware platform (if the bus to the memory is wider then caching needs to be implemented in the framework to make use of it). Inputs of the algorithm implemented in Hast_IP all come through this port.
-    -- * DataOut: Data to be written to the memory is assigned to this port. The width of this port is always 32b, independent of the hardware platform (if the bus to the memory is wider then caching needs to be implemented in the framework to make use of it). Outputs of the algorithm implemented in Hast_IP all go through this port.
-    -- * CellIndex: Zero-based index of the SimpleMemory memory cell currently being read or written. Transformed code in Hastlayer can access memory in a simplified fashion by addressing 32b "cells", the accessible physical memory space being divided up in such individually addressable cells.
+    -- * DataIn: Data read out from the memory (usually on-board DDR RAM, but depends on the framework) should be assigned to
+    --           this port by the framework. The width of this port is always 32b, independent of the hardware platform (if the
+    --           bus to the memory is wider then caching needs to be implemented in the framework to make use of it). Inputs of
+    --           the algorithm implemented in Hast_IP all come through this port.
+    -- * DataOut: Data to be written to the memory is assigned to this port. The width of this port is always 32b, independent
+    --            of the hardware platform (if the bus to the memory is wider then caching needs to be implemented in the
+    --            framework to make use of it). Outputs of the algorithm implemented in Hast_IP all go through this port.
+    -- * CellIndex: Zero-based index of the SimpleMemory memory cell currently being read or written. Transformed code in
+    --              Hastlayer can access memory in a simplified fashion by addressing 32b "cells", the accessible physical
+    --              memory space being divided up in such individually addressable cells.
     -- * ReadEnable: Indicates whether a memory read operation is initiated. The process of a memory read is as following:
-    --     1. ReadEnable is FALSE by default. It's set to TRUE when a memory read is started. CellIndex is set to the index of the memory cell to be read out.
+    --     1. ReadEnable is FALSE by default. It's set to TRUE when a memory read is started. CellIndex is set to the index of
+    --        the memory cell to be read out.
     --     2. Waiting for ReadsDone to be TRUE.
     --     3. Once ReadsDone is TRUE, data from DataIn will be read out and ReadEnable set to FALSE.
     -- * WriteEnable: Indicates whether a memory write operation is initiated. The process of a memory write is as following:
-    --     1. WriteEnable is FALSE by default. It's set to TRUE when a memory write is started. CellIndex is set to the index of the memory cell to be written and the output data is assigned to DataOut.
+    --     1. WriteEnable is FALSE by default. It's set to TRUE when a memory write is started. CellIndex is set to the index
+    --        of the memory cell to be written and the output data is assigned to DataOut.
     --     2. Waiting for WritesDone to be TRUE.
     --     3. Once WritesDone is TRUE, WriteEnable is set to FALSE.
     -- * ReadsDone: Indicates whether a memory read operation is completed.
@@ -9045,15 +9054,24 @@ entity Hast_IP is
     --             the Started port above on how it is used.
     -- * Reset: Synchronous reset.
     -- * Clock: The main clock.
-    -- * DataIn: Data read out from the memory (usually on-board DDR RAM, but depends on the framework) should be assigned to this port by the framework. The width of this port is always 32b, independent of the hardware platform (if the bus to the memory is wider then caching needs to be implemented in the framework to make use of it). Inputs of the algorithm implemented in Hast_IP all come through this port.
-    -- * DataOut: Data to be written to the memory is assigned to this port. The width of this port is always 32b, independent of the hardware platform (if the bus to the memory is wider then caching needs to be implemented in the framework to make use of it). Outputs of the algorithm implemented in Hast_IP all go through this port.
-    -- * CellIndex: Zero-based index of the SimpleMemory memory cell currently being read or written. Transformed code in Hastlayer can access memory in a simplified fashion by addressing 32b "cells", the accessible physical memory space being divided up in such individually addressable cells.
+    -- * DataIn: Data read out from the memory (usually on-board DDR RAM, but depends on the framework) should be assigned to
+    --           this port by the framework. The width of this port is always 32b, independent of the hardware platform (if the
+    --           bus to the memory is wider then caching needs to be implemented in the framework to make use of it). Inputs of
+    --           the algorithm implemented in Hast_IP all come through this port.
+    -- * DataOut: Data to be written to the memory is assigned to this port. The width of this port is always 32b, independent
+    --            of the hardware platform (if the bus to the memory is wider then caching needs to be implemented in the
+    --            framework to make use of it). Outputs of the algorithm implemented in Hast_IP all go through this port.
+    -- * CellIndex: Zero-based index of the SimpleMemory memory cell currently being read or written. Transformed code in
+    --              Hastlayer can access memory in a simplified fashion by addressing 32b "cells", the accessible physical
+    --              memory space being divided up in such individually addressable cells.
     -- * ReadEnable: Indicates whether a memory read operation is initiated. The process of a memory read is as following:
-    --     1. ReadEnable is FALSE by default. It's set to TRUE when a memory read is started. CellIndex is set to the index of the memory cell to be read out.
+    --     1. ReadEnable is FALSE by default. It's set to TRUE when a memory read is started. CellIndex is set to the index of
+    --        the memory cell to be read out.
     --     2. Waiting for ReadsDone to be TRUE.
     --     3. Once ReadsDone is TRUE, data from DataIn will be read out and ReadEnable set to FALSE.
     -- * WriteEnable: Indicates whether a memory write operation is initiated. The process of a memory write is as following:
-    --     1. WriteEnable is FALSE by default. It's set to TRUE when a memory write is started. CellIndex is set to the index of the memory cell to be written and the output data is assigned to DataOut.
+    --     1. WriteEnable is FALSE by default. It's set to TRUE when a memory write is started. CellIndex is set to the index
+    --        of the memory cell to be written and the output data is assigned to DataOut.
     --     2. Waiting for WritesDone to be TRUE.
     --     3. Once WritesDone is TRUE, WriteEnable is set to FALSE.
     -- * ReadsDone: Indicates whether a memory read operation is completed.
