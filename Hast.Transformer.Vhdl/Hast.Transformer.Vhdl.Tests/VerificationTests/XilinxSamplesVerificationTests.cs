1b24195243b0f8240b38a0169715e145d42ec3bf
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-26 14:25:19 +0100

    Fix unnecessary double testing of Nexys4.

--- Hast.Transformer.Vhdl/Hast.Transformer.Vhdl.Tests/VerificationTests/XilinxSamplesVerificationTests.cs
+++ Hast.Transformer.Vhdl/Hast.Transformer.Vhdl.Tests/VerificationTests/XilinxSamplesVerificationTests.cs
@@ -21,47 +21,38 @@ namespace Hast.Transformer.Vhdl.Tests.VerificationTests
             (await CreateSourceForBasicSamplesAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
 
         [Theory, MemberData(nameof(AllDevices))]
-        [InlineData(Nexys4DdrManifestProvider.DeviceName)]
         public async Task KpzSamplesMatchesApproved(string deviceName) =>
             (await CreateVhdlForKpzSamplesAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
 
         [Theory, MemberData(nameof(AllDevices))]
-        [InlineData(Nexys4DdrManifestProvider.DeviceName)]
         public async Task UnumSampleMatchesApproved(string deviceName) =>
             (await CreateVhdlForUnumSampleAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
 
         [Theory, MemberData(nameof(AllDevices))]
-        [InlineData(Nexys4DdrManifestProvider.DeviceName)]
         public async Task PositSampleMatchesApproved(string deviceName) =>
             (await CreateVhdlForPositSampleAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
 
         [Theory, MemberData(nameof(AllDevices))]
-        [InlineData(Nexys4DdrManifestProvider.DeviceName)]
         public async Task Posit32SampleMatchesApproved(string deviceName) =>
             (await CreateVhdlForPosit32SampleAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
 
         [Theory, MemberData(nameof(AllDevices))]
-        [InlineData(Nexys4DdrManifestProvider.DeviceName)]
         public async Task Posit32AdvancedSampleMatchesApproved(string deviceName) =>
             (await CreateSourceForAdvancedPosit32SampleAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
 
         [Theory, MemberData(nameof(AllDevices))]
-        [InlineData(Nexys4DdrManifestProvider.DeviceName)]
         public async Task Posit32SampleWithInliningMatchesApproved(string deviceName) =>
             (await CreateVhdlForPosit32SampleWithInliningAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
 
         [Theory, MemberData(nameof(AllDevices))]
-        [InlineData(Nexys4DdrManifestProvider.DeviceName)]
         public async Task Posit32FusedSampleMatchesApproved(string deviceName) =>
             (await CreateVhdlForPosit32FusedSampleAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
 
         [Theory, MemberData(nameof(AllDevices))]
-        [InlineData(Nexys4DdrManifestProvider.DeviceName)]
         public async Task Fix64SamplesMatchesApproved(string deviceName) =>
             (await CreateVhdlForFix64SamplesAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
 
         [Theory, MemberData(nameof(AllDevices))]
-        [InlineData(Nexys4DdrManifestProvider.DeviceName)]
         public async Task FSharpSamplesMatchesApproved(string deviceName) =>
             (await CreateVhdlForFSharpSamplesAsync()).ShouldMatchApprovedWithVhdlConfiguration(deviceName);
     }
